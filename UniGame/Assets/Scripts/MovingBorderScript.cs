db7af3cdfc0fb42105b16b53402e6a77bfbf8ad3
William Djalal <william.dj@outlook.de>
2017-11-01 16:21:42 +0100

    New Version (PC)

---
+++ UniGame/Assets/Scripts/MovingBorderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBorderScript : MonoBehaviour {
+    public int Border;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Border == 0)
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Border == 1)
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+    }
+}
---
+++ UniGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public int Player;
+    public int dir;
+    public float force;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DirChanger")
+        {
+            if (dir == 0)
+            {
+                dir = 1;
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                dir = 0;
+                Destroy(other.gameObject);
+            }
+        }
+
+        if (other.gameObject.tag == "ColBlock")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (other.gameObject.tag == "Barrier")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        if (dir == 1)
+        {
+            rb.AddForce(new Vector3(force * Time.deltaTime, 0, 0));
+        }
+        if (dir == 0)
+        {
+            rb.AddForce(new Vector3(-force * Time.deltaTime, 0, 0));
+        }
+
+        if (Player == 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+        }
+        if (Player == 1)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+        }
+    }
+}
